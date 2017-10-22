3ab726613028c8e6e97acc7f563e012c5d738a79
David Pfeiffer <davepfeifferer@gmail.com>
2017-10-21 23:05:14 -0400

    time alias and tex template alias

---
+++ mkdoc.fish
@@ -0,0 +1,6 @@
+#
+# Creates an assembly file from a specific template
+#
+function mkdoc
+  command cp ~/Templates/doc.tex ./$argv
+end
---
+++ time.fish
@@ -0,0 +1,15 @@
+#
+# Creates an assembly file from a specific template
+#
+function time
+  command time -f \
+"
++--------------------+
+| Runtime Statistics |
++---------+----------+
+| Elapsed | %E  |
+| System  | %S     |
+| User    | %U     |
++---------+----------+
+" ./$argv
+end
