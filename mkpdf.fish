ae8a972ffd08af102585492d921f957a64810b70
David Pfeiffer <davepfeifferer@gmail.com>
2017-12-06 16:14:49 -0500

    add bibtex support to mkpdf

--- mkpdf.fish
+++ mkpdf.fish
@@ -3,22 +3,23 @@
 #
 # *intended to be used with texlive
 function mkpdf
-    mkdir ./output ^ /dev/null
-    begin
-    	if echo $argv | grep '.*\.tex$' > /dev/null
-	    	set name (echo $argv | sed 's/\.[^.]*$//')
-	        set flags \
-	            -halt-on-error -output-directory=./output
-	        if xelatex $flags $argv > /dev/null ^ /dev/null
-	            evince ./output/$name.pdf > /dev/null ^ /dev/null &
-	        else
-	            cat ./output/$name.log
-	        end
-	    else
-	    	set_color $fish_color_error
-	    	echo -n "invalid parameter: "
-	    	set_color $fish_color_normal
-	    	echo "*.tex file expected"
-	    end
+  mkdir ./output ^ /dev/null
+  begin
+  	if echo $argv | grep '.*\.tex$' > /dev/null
+    	set name (echo $argv | sed 's/\.[^.]*$//')
+      set flags -halt-on-error -output-directory=./output
+      if xelatex $flags $argv > /dev/null ^ /dev/null
+        bibtex ./output/$name.aux > /dev/null ^ /dev/null
+        xelatex $flags $argv > /dev/null ^ /dev/null
+        evince ./output/$name.pdf > /dev/null ^ /dev/null &
+      else
+        cat ./output/$name.log
+      end
+    else
+    	set_color $fish_color_error
+    	echo -n "invalid parameter: "
+    	set_color $fish_color_normal
+    	echo "*.tex file expected"
     end
+  end
 end
